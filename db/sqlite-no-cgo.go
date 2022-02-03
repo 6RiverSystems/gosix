2c4fdd45bb91adcdc6949b458205ea24ec87ce4d
Matthew Gabeler-Lee <mgabeler-lee@6river.com>
2022-02-02 19:10:04 -0500

    fix: memory support, select sqlite driver based on cgo

--- db/sqlite-no-cgo.go
+++ db/sqlite-no-cgo.go
@@ -1,7 +1,32 @@
+// Copyright (c) 2022 6 River Systems
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 //go:build !cgo
 // +build !cgo
 
 package db
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
 
 // in non-CGO mode, use the modernc.org/sqlite driver
 const sqliteDriver = "sqlite"
@@ -24,12 +49,13 @@ func SQLiteDSN(filename string, fileScheme, memory bool) string {
 			"foreign_keys(1)",
 			"journal_mode(wal)",
 			"busy_timeout(10000)",
-		}
-		// TODO: https://gitlab.com/cznic/sqlite/-/issues/92
+		},
+		// ref: https://gitlab.com/cznic/sqlite/-/issues/92
 		// we need BEGIN IMMEDIATE for several use cases to work
 		"_txlock": []string{"immediate"},
 	}
 	if memory {
+		filename = ":memory:"
 		// memory mode needs either shared cache, or single connection. shared cache
 		// doesn't play nice and results in lots of unfixable "table is locked"
 		// errors, so instead rely on `Open` to set the max conns appropriately
