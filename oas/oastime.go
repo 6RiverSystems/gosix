fbaacbc95c0ccf3e3eb0d9f7c65b57c75f03cc78
Matthew Gabeler-Lee <mgabeler-lee@6river.com>
2024-01-17 01:55:10 -0500

    fix: make oas.time not confusible for oapi-codegen types.Date

--- oas/oastime.go
+++ oas/oastime.go
@@ -25,29 +25,36 @@ import (
 )
 
 type Time struct {
+	// this hidden prop is to prevent it from being convertible to time.Date
+	_ struct{}
 	time.Time
 }
 
 const OASRFC3339Millis = "2006-01-02T15:04:05.000Z07:00"
 
 func Now() Time {
-	return Time{time.Now().UTC().Truncate(time.Millisecond)}
+	return Time{Time: time.Now().UTC().Truncate(time.Millisecond)}
 }
 
 func FromTime(value time.Time) Time {
-	return Time{value.UTC().Truncate(time.Millisecond)}
+	return Time{Time: value.UTC().Truncate(time.Millisecond)}
 }
 
 func FromTimePtr(value *time.Time) *Time {
 	if value == nil {
 		return nil
 	}
-	return &Time{value.UTC().Truncate(time.Millisecond)}
+	return &Time{Time: value.UTC().Truncate(time.Millisecond)}
 }
 
 func (t Time) MarshalJSON() ([]byte, error) {
 	// TODO: pre-allocate to make it faster
 	return []byte(fmt.Sprintf(`"%s"`, t.UTC().Format(OASRFC3339Millis))), nil
+}
+
+func (t Time) MarshalText() ([]byte, error) {
+	// TODO: pre-allocate to make it faster
+	return []byte(t.UTC().Format(OASRFC3339Millis)), nil
 }
 
 // Bind implements github.com/oapi-codegen/runtime.Binder, to prevent it from
